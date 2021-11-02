6ad44f37d33ea9532aed4684dda9a65f80a4175f
Raul <raul.turc@stud.ubbcluj.ro>
2021-11-02 07:56:58 +0200

    Modificarile

--- Tests/testAll.py
+++ Tests/testAll.py
@@ -1,9 +1,12 @@
-from Tests.test_CRUD import test_sterge,test_adaugacomanda,test_getbyID,test_modif_comanda
+from Tests.test_CRUD import test_sterge,test_adaugacomanda,test_getbyID,test_modif_comanda,test_modif_gen,test_discount
 from Tests.testDomain import testComanda
 
 
 def runAlltests() -> object:
     l=[]
+    test_modif_gen()
+    test_discount()
+    test_getbyID()
     testComanda()
     test_adaugacomanda(l)
     test_sterge()
--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     l = adaugacomanda("1", "Piratii din Caraibe", 45, "Explorare", "Gold",l)
     l = adaugacomanda("2", "Alba ca zapada", 25, "Basm", "Silver",l)
     l = adaugacomanda("3","Cenusareasa",10,"Basm","Gold",l)
+    print("1)Interfata clasica:")
+
+    optiune =input("Dati codul de interfata pe care doriti sa o accesati: ")
     command_line_console(l)
 
 main()
